8ed1c2ba40bbe5c0ce85fff7d56a7f48569644ab
skytech001 <abiodunlabinjo@gmail.com>
2022-11-23 18:03:58 -0600

    Initial commit for marketPlace app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Centerspade
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
       </header>
+
+      <main>
+        <div className="row center">
+          {data.products.map((product) => {
+            return (
+              <div key={product.id} className="card">
+                <a href={`/product/${product.id}`}>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product.id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star-o"></i>
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
